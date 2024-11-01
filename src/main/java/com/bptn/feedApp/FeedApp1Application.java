7478d15e2935a4f607d84aff8f23cd978669ef99
olaomoju lawal <olaomojulawal@gmail.com>
2024-11-01 16:04:36 -0400

    Initial commit

---
+++ src/main/java/com/bptn/feedApp/FeedApp1Application.java
@@ -0,0 +1,13 @@
+package com.bptn.feedApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bptn/feedApp/FeedApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bptn.feedApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
