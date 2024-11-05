5ffe8efe8f6f931e1164411753f2b8fe9fe1dfff
olaomoju lawal <olaomojulawal@gmail.com>
2024-11-05 11:47:35 -0500

    UserController

--- src/main/java/com/bptn/feedApp/controller/UserController.java
+++ src/main/java/com/bptn/feedApp/controller/UserController.java
@@ -1,21 +1,38 @@
 package com.bptn.feedApp.controller;
 
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bptn.feedApp.jdbc.UserBean;
+import com.bptn.feedApp.service.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
- final Logger logger = LoggerFactory.getLogger(this.getClass());
- 
- @GetMapping("/test")
- public String testController() {
-  logger.debug("The testController() method was invoked!");
-  return "The FeedApp application is up and running";
- }
+	// final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@GetMapping("/test")
+	public String testController() {
+		logger.debug("The testController() method was invoked!");
+		return "The FeedApp application is up and running";
+	}
+
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	UserService userService;
+
+	@GetMapping("/")
+	public List<UserBean> listUsers() {
+		logger.debug("The listUsers() method was invoked!");
+		return this.userService.listUsers();
+
+	}
+
 }
