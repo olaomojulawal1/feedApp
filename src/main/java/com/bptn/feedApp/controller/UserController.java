2a56e5d8fde66582341bd6d5311705570f9ab457
olaomoju lawal <olaomojulawal@gmail.com>
2024-11-05 14:20:42 -0500

    UserController

--- src/main/java/com/bptn/feedApp/controller/UserController.java
+++ src/main/java/com/bptn/feedApp/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.bptn.feedApp.controller;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +35,33 @@ public class UserController {
 	public List<UserBean> listUsers() {
 		logger.debug("The listUsers() method was invoked!");
 		return this.userService.listUsers();
-
+	}
+	
+	@GetMapping("/{username}") //url parameter
+	 public UserBean findByUsername(@PathVariable String username) {
+	  logger.debug("The findByUsername() method was invoked!, username={}", username);
+	  return this.userService.findByUsername(username);
+	 }
+	
+	@GetMapping("/{first}/{last}/{username}/{password}/{phone}/{emailId}")
+	 public String createUser(@PathVariable String first, @PathVariable String last, @PathVariable String username,
+	   @PathVariable String password, @PathVariable String phone, @PathVariable String emailId)  {
+	   UserBean user = new UserBean();
+	      
+	   user.setFirstName(first);
+	   user.setLastName(last);
+	   user.setUsername(username);
+	   user.setPassword(password);
+	   user.setPhone(phone);
+	   user.setEmailId(emailId);
+	   user.setEmailVerified(false);
+	   user.setCreatedOn(Timestamp.from(Instant.now()));
+	           
+	   logger.debug("The createUser() method was invoked!, user={}", user.toString());
+	           
+	   this.userService.createUser(user);
+	           
+	   return "User Created Successfully";
 	}
 
 }
